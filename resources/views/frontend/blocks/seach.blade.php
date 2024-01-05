b9133e15a2f90f5c688b2144140039ba883b348a
LongKunno <longdd@liftsoft.vn>
2024-01-06 00:50:37 +0700

    cm

--- resources/views/frontend/blocks/seach.blade.php
+++ resources/views/frontend/blocks/seach.blade.php
@@ -1,7 +1,7 @@
 <div class="aa-search-box" >
 	<form action="{!! route('getTimkiem') !!}" method="POST">
     	<input type="hidden" name="_token" value="{!! csrf_token() !!}" />
-		<input type="text" name="txtSeach" id="txtseach" placeholder="Tìm kiếm..." pattern="[A-Za-z0-9\s]+" title="Không cho phép nhập dấu">
+		<input type="text" name="txtSeach" id="txtseach" placeholder="Tìm kiếm..." pattern="[A-Za-z0-9]+" title="Không cho phép nhập dấu">
 	  	<button type="submit"><span class="fa fa-search"></span></button>
 	</form>
 </div>
